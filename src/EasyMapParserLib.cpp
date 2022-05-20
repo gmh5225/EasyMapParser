ae55fd29ba6c548fea1d85af2730b782abfc6230
gmh5225 <2315157@qq.com>
2022-05-20 20:42:31 +0800

    Update

--- src/EasyMapParserLib.cpp
+++ src/EasyMapParserLib.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <ostream>
+#include <regex>
 
 namespace EasyMapParserLib {
 
@@ -22,6 +23,32 @@ MapParser::GetSymbols() const
 bool
 MapParser::Parse(const char *MapFilePath)
 {
+    auto ParseSymbol = [&](std::string &Line) {
+        std::regex Reg(
+            R"#(\s(\d+)\:([a-fA-F0-9]+)\s+(\S+)\s+([a-fA-F0-9]+)\s+(.+))#", std::regex_constants::ECMAScript);
+        if (std::regex_match(Line, Reg))
+        {
+            std::smatch Match;
+            for (; std::regex_search(Line, Match, Reg); Line = Match.suffix())
+            {
+                Symbol Sym = {0};
+                Sym.Address = std::strtoull(Match[4].str().c_str(), nullptr, 16);
+
+                if (Sym.Address == 0)
+                {
+                    continue;
+                }
+
+                Sym.SectionIndex = std::atoi(Match[1].str().c_str());
+                Sym.SectionOffset = std::strtoul(Match[2].str().c_str(), nullptr, 16);
+                Sym.SymbolName = Match[3].str();
+                Sym.LibObjName = Match[5].str();
+
+                mSymbols.emplace_back(Sym);
+            }
+        }
+    };
+
     if ((access(MapFilePath, 0) != 0))
     {
         // file does not exist
@@ -30,9 +57,11 @@ MapParser::Parse(const char *MapFilePath)
 
     std::ifstream IfStream(MapFilePath);
     std::string Line = "";
+
     while (std::getline(IfStream, Line))
     {
         // read line
+        ParseSymbol(Line);
     }
 
     if (mSymbols.empty())
