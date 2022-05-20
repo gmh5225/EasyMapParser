fe5c37437ad00fe50fc5c7bc1d6a99486a819289
gmh5225 <2315157@qq.com>
2022-05-20 20:50:07 +0800

    Update

---
+++ configurations.cmake
@@ -0,0 +1,9 @@
+# set output binary dir
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Output)
+
+# add source directory macro
+add_definitions(-DEASYMAPPARSER_SRC_DIR="${CMAKE_CURRENT_SOURCE_DIR}")
+
+
+
+
